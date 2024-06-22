0ab9d56ba3ca5d002577e8efd35a3a1d99ffb274
Mehmet Öcal <mehmettoocal@gmail.com>
2024-06-22 15:04:51 +0300

    Product Details Screen

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,14 +1,91 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
 import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams } from "expo-router";
+import products from "@assets/data/products";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+import { useState } from "react";
+import Button from "@components/Button";
+
+const sizes = ["S", "L", "M", "XL"];
 
 const ProductDetailScreen = () => {
   const { id } = useLocalSearchParams();
+
+  const [selectedSize, setSelectedSize] = useState("M");
+
+  const product = products.find((p) => p.id.toString() === id);
+
+  const addToCart = () => {
+    console.warn("Adding , size:", selectedSize);
+  };
+
+  if (!product) {
+    return <Text>Product not found</Text>;
+  }
+
   return (
-    <View>
-      <Text>ProductDetailScreen: {id}</Text>
+    <View style={style.container}>
+      <Stack.Screen options={{ title: product?.name }} />
+      <Image source={{ uri: product.image || defaultPizzaImage }} style={style.image} />
+      <Text>Select size</Text>
+
+      <View style={style.sizes}>
+        {sizes.map((size) => (
+          <Pressable
+            onPress={() => {
+              setSelectedSize(size);
+            }}
+            key={size}
+            style={[
+              style.size,
+              {
+                backgroundColor: selectedSize == size ? "gainsboro" : "white",
+              },
+            ]}
+          >
+            <Text style={[style.sizeText, { color: selectedSize == size ? "black" : "gray" }]}>{size}</Text>
+          </Pressable>
+        ))}
+      </View>
+
+      <Text style={style.price}>${product.price}</Text>
+      <Button onPress={addToCart} text="Add to Cart" />
     </View>
   );
 };
+
+const style = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  sizes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 10,
+  },
+  size: {
+    backgroundColor: "gainsboro",
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  sizeText: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: "auto",
+  },
+});
 
 export default ProductDetailScreen;
